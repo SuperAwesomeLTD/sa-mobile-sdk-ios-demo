57ecd95fc77f11501b0cbe5c04ab5c938c1f8864
Gabriel Coman <dev.gabriel.coman@gmail.com>
2017-06-29 11:30:46 +0100

    update

--- AwesomeAdsDemo/AppDelegate.swift
+++ AwesomeAdsDemo/AppDelegate.swift
@@ -15,7 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.        
+        // Override point for customization after application launch.  
+        
+        let view = UIView(frame: CGRect(x: 0.0, y: 0.0, width: UIScreen.main.bounds.size.width, height: 20.0))
+        view.backgroundColor = UIColor(colorLiteralRed: 108.0/255.0, green: 157.0/255.0, blue: 252.0/255.0, alpha: 1)
+        self.window?.rootViewController?.view.addSubview(view)
+        
         return true
     }
 
